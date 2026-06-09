92622904b67a9021c136898e7bf254a873989dd5
Angelsan22 <123048340@upq.edu.mx>
2026-06-09 10:23:21 -0600

    se creo la practica 6 en donde se nos enseño como es que funcionan los componentes de una pagina movil y la visualizacion de esta misma

--- Practica5/miapp2/App.js
+++ Practica5/miapp2/App.js
@@ -1,15 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
+
+/* Zona1: Importaciones componentes y archivos*/
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { Saludo } from './components/Saludo';
+import { Saludo2 } from './components/Saludo2';
+import { Perfil } from './components/Perfil';
+
+
+
+/* Zona2: Main - Componentes*/
 export default function App() {
   return (
+    
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <Image source={require('./assets/wave.png')}/>
+      <Text>Hola Mundo RN</Text>
+        <Text>-------------------------------------</Text>
+
+      <Saludo />
+      <Saludo></Saludo>
+        <Text>-------------------------------------</Text>
+
+      <Saludo2/>
+
+        <Text>-------------------------------------</Text>
+      
+      <Perfil/>
+      
       <StatusBar style="auto" />
+    
     </View>
+
   );
 }
 
+/* Zona3: Estilos y Posicionamientos*/
 const styles = StyleSheet.create({
   container: {
     flex: 1,
---
+++ Practica5/miapp2/components/Perfil.js
@@ -0,0 +1,16 @@
+import { Text, View } from 'react-native';
+
+export const Perfil = () => {
+    return (
+            <View>
+
+                <Text>Nombre: Sánchez Liares José Ángel</Text>
+                <Text>Carrera: Ingeniería en Sistemas Computacionales</Text>
+                <Text>Materia: Programación Móvil</Text>
+                <Text>Cuatrimestre: 9no</Text>
+
+            </View>
+
+    )
+}
+    
